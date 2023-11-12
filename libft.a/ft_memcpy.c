0abc5b8111debb49142bf2d8cfbd475e160d18f2
Haggarot <nbianchi.agronomo@gmail.com>
2023-11-12 11:00:19 +0100

    Avanzamento Domenica 12/11

--- libft.a/ft_memcpy.c
+++ libft.a/ft_memcpy.c
@@ -1,9 +1,19 @@
-#include <libft.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbianchi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/12 10:45:37 by nbianchi          #+#    #+#             */
+/*   Updated: 2023/11/12 10:56:06 by nbianchi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void memcpy(void *dest, const void *src, size_t n)
+void    memcpy(void *dest, const void *src, size_t n)
 {
-    int i;
-    char tmp[];
+    int     i;
+    char    tmp[];
 
     i = 0;
 
--- libft.a/libft.h
+++ libft.a/libft.h
@@ -1,5 +1,17 @@
-#ifndef libft_h
-#define libft_h
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nbianchi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/12 10:46:20 by nbianchi          #+#    #+#             */
+/*   Updated: 2023/11/12 10:59:48 by nbianchi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
 
 #include <unistd.h>
 #include <stdio.h>
@@ -23,11 +35,16 @@ void	ft_putnchar(char c, int n);
 /*MEMORY*/
 void    ft_memset(void *s, int c, size_t n);
 void    ft_bzero(void *s, size_t n);
+void    memcpy(void *dest, const void *src, size_t n);
+int 	memcmp(const void *s1, const void *s2, size_t n);
+void 	*memchr(const void *s, int c, size_t n);
 
 /*STRING*/
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
-int     strncmp(const char *s1, const char *s2, size_t n);
+int 	strncmp(const char *s1, const char *s2, size_t n);
+char 	*strrchr(const char *s, int c);
+
 
 /*CHAR*/
 int	ft_toupper(int c);
