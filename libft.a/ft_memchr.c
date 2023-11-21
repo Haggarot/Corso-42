ba50aadc83afc9a1da4978e1ab39a6498787af9a
Haggarot <nbianchi.agronomo@gmail.com>
2023-11-21 22:01:24 +0100

    Update ft_memchr.c

--- libft.a/ft_memchr.c
+++ libft.a/ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: nbianchi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/12 10:52:14 by nbianchi          #+#    #+#             */
-/*   Updated: 2023/11/13 21:15:47 by nbianchi         ###   ########.fr       */
+/*   Updated: 2023/11/13 21:22:50 by nbianchi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		if (((unsigned char *)s)[i] == (unsigned char *)c)
+		if ((unsigned char *)s)[i] == ((unsigned char *)c)
 			return (((unsigned char *)s) + i);
 		else
 			i++;
